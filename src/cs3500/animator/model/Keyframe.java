f98790fd52a7eaab18e37e808cc7eac7eb1583b7
meieri <meier.i@husky.neu.edu>
2019-04-10 20:36:34 -0400

    Initial commit

---
+++ src/cs3500/animator/model/Keyframe.java
@@ -0,0 +1,14 @@
+package cs3500.animator.model;
+
+import cs3500.animator.model.motion.Motion;
+
+public class Keyframe {
+  Motion m;
+
+  Keyframe(Motion m, String name) {
+    this.m = m;
+  }
+
+
+
+}
